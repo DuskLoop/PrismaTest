ede0c20bec8ea301c46e5c110dad67b906c15131
Daniel Hildesson <daniel.hildesson@eniac.se>
2022-02-15 17:06:04 +0100

    Changed to sql-server

--- data/links.ts
+++ data/links.ts
@@ -21,7 +21,7 @@ export const links = [
     description: "Utility-fist css framework",
     id: "6a9122b2-363b-4a4f-ad26-d6c55b51baed",
     imageUrl:
-      "https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg",
+      "https://v1.tailwindcss.com/_next/static/media/tailwindcss-logotype.5d127e1d767d06efa2b03fe4de5f3d84.svg",
     title: "TailwindCSS",
     url: "https://tailwindcss.com",
   },
